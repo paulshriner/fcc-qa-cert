fe8a1aad1ea51f39e35ddb6270d5bc9793725c00
paulshriner <shrinerpaul@gmail.com>
2025-06-02 19:14:51 -0400

    american-british-translator: translate words and spellings

--- american-british-translator/components/translator.js
+++ american-british-translator/components/translator.js
@@ -4,7 +4,42 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
+    translate(str, mode) {
+        let translatedString = str;
 
+        if (mode == 'american-to-british') {
+            // translate direct word changes
+            for (const word in americanOnly) {
+                // thanks https://stackoverflow.com/a/50828436 for regex
+                // used boundaries (\\b) so that a word within a word isn't detected
+                // case insensitive and global to get all occurrences
+                let regex = new RegExp(`\\b${word}\\b`, 'gi');
+                translatedString = translatedString.replace(regex, '<span class="highlight">'+ americanOnly[word] + '</span>');
+            }
+
+            // translate word spellings
+            for (const word in americanToBritishSpelling) {
+                let regex = new RegExp(`\\b${word}\\b`, 'gi');
+                translatedString = translatedString.replace(regex, '<span class="highlight">'+ americanToBritishSpelling[word] + '</span>');
+            }
+        } else if (mode == 'british-to-american') {
+            // both direct words and spellings are just opposite of american-to-british
+            for (const word in britishOnly) {
+                let regex = new RegExp(`\\b${word}\\b`, 'gi');
+                translatedString = translatedString.replace(regex, '<span class="highlight">'+ britishOnly[word] + '</span>');
+            }
+
+            for (const word in americanToBritishSpelling) {
+                let regex = new RegExp(`\\b${americanToBritishSpelling[word]}\\b`, 'gi');
+                translatedString = translatedString.replace(regex, '<span class="highlight">'+ word + '</span>');
+            }
+        }
+
+        // if translatedString is the same then nothing is translated
+        if (translatedString == str) return "Everything looks good to me!";
+
+        return translatedString;
+    }
 }
 
 module.exports = Translator;
--- american-british-translator/routes/api.js
+++ american-british-translator/routes/api.js
@@ -2,12 +2,22 @@
 
 const Translator = require('../components/translator.js');
 
-module.exports = function (app) {
-  
+module.exports = app => {
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      if (req.body.text == undefined || req.body.locale == undefined) {
+        res.json({error: 'Required field(s) missing'});
+      } else if (req.body.text == '') {
+        res.json({error: 'No text to translate'});
+      } else if (req.body.locale != 'american-to-british' && req.body.locale != 'british-to-american') {
+        res.json({error: 'Invalid value for locale field'})
+      } else {
+        res.json({
+          text: req.body.text,
+          translation: translator.translate(req.body.text, req.body.locale)
+        });
+      }
     });
 };
