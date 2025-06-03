ead918157b84b149567c5c1ee3c0a4955f62a23e
paulshriner <shrinerpaul@gmail.com>
2025-06-02 21:55:57 -0400

    american-british-translator: time, title translation

--- american-british-translator/components/translator.js
+++ american-british-translator/components/translator.js
@@ -14,24 +14,48 @@ class Translator {
                 // used boundaries (\\b) so that a word within a word isn't detected
                 // case insensitive and global to get all occurrences
                 let regex = new RegExp(`\\b${word}\\b`, 'gi');
-                translatedString = translatedString.replace(regex, '<span class="highlight">'+ americanOnly[word] + '</span>');
+                translatedString = translatedString.replace(regex, this.highlightText(americanOnly[word]));
             }
 
             // translate word spellings
             for (const word in americanToBritishSpelling) {
                 let regex = new RegExp(`\\b${word}\\b`, 'gi');
-                translatedString = translatedString.replace(regex, '<span class="highlight">'+ americanToBritishSpelling[word] + '</span>');
+                translatedString = translatedString.replace(regex, this.highlightText(americanToBritishSpelling[word]));
             }
+
+            // translate time
+            // thanks https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace for tip on callback
+            translatedString = translatedString.replace(/\d+:\d+/g, match => this.highlightText(match.replace(':', '.')));
+
+            // translate titles
+            translatedString = translatedString.replace(/[A-Za-z]+./gi, match => {
+                for (const word in americanToBritishTitles) {
+                    if (match.toLowerCase() == word.toLowerCase()) {
+                        return this.highlightText(match.replace('.', ''));
+                    }
+                }
+
+                return match;
+            });
         } else if (mode == 'british-to-american') {
             // both direct words and spellings are just opposite of american-to-british
             for (const word in britishOnly) {
                 let regex = new RegExp(`\\b${word}\\b`, 'gi');
-                translatedString = translatedString.replace(regex, '<span class="highlight">'+ britishOnly[word] + '</span>');
+                translatedString = translatedString.replace(regex, this.highlightText(britishOnly[word]));
             }
 
             for (const word in americanToBritishSpelling) {
                 let regex = new RegExp(`\\b${americanToBritishSpelling[word]}\\b`, 'gi');
-                translatedString = translatedString.replace(regex, '<span class="highlight">'+ word + '</span>');
+                translatedString = translatedString.replace(regex, this.highlightText(word));
+            }
+
+            // translate time
+            translatedString = translatedString.replace(/\d+.\d+/g, match => this.highlightText(match.replace('.', ':')));
+
+            // translate titles
+            for (const word in americanToBritishTitles) {
+                let regex = new RegExp(`\\b${americanToBritishTitles[word]}\\b`, 'gi');
+                translatedString = translatedString.replace(regex, this.highlightText(word));                
             }
         }
 
@@ -39,6 +63,11 @@ class Translator {
         if (translatedString == str) return "Everything looks good to me!";
 
         return translatedString;
+    }
+
+    // Helper function that highlights a string given
+    highlightText(str) {
+        return '<span class="highlight">' + str + '</span>';
     }
 }
 
